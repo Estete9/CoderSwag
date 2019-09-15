55dbefe33c4cd383283bbf9f5e301a344629f6eb
Esteban P <cepalacios@outlook.com>
2019-09-14 21:35:16 -0500

    almost made the recycler view adaptergit add .

--- app/src/main/java/com/example/coderswag/Controllers/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controllers/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.coderswag.Controllers
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coderswag.Models.Category
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: RecyclerView.Adapter <RecyclerView.ViewHolder>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = RecyclerView.Adapter<RecyclerView.ViewHolder(this, )/*ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)*/
+
+        categoryRecyclerView.adapter = adapter
     }
 }
